27543359d3ec3450cc05ec6fd6b1a0db1fdf1be0
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-11 20:53:47 +0530

    feat: added more card items; fix: best score fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const [selectedCards, setSelectedCards] = useState([]);
   const [currScore, setCurrScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   const [cards, setCards] = useState([
     {
       text: "Lamborgini",
@@ -22,7 +23,27 @@ function App() {
     {
       text: "Porsche",
       img: "https://www.carlogos.org/car-logos/porsche-logo-950x1100.png"
-    }
+    },
+    {
+      text: "Rolls-Royce",
+      img: "https://lezebre.lu/images/detailed/70/17246-rolls-royce-logo-3.png"
+    },
+    {
+      text: "Ford",
+      img: "https://1000logos.net/wp-content/uploads/2018/02/Ford-Logo.png"
+    },
+    {
+      text: "Bentley",
+      img: "https://audimediacenter-a.akamaihd.net/system/production/media/28400/images/8e25f6bd8579b61c1051a52650813ebf676313dc/A160053_full.jpg?1582245503",
+    },
+    {
+      text: "McLaren",
+      img: "https://formularapida.net/wp-content/uploads/2019/01/DxG7bOQXgAEul7l.jpg"
+    },
+    {
+      text: "Buggati",
+      img: "https://www.carlogos.org/logo/Bugatti-logo-2560x1440.png"
+    },
   ]);
 
   const shuffleCards = () => {
@@ -44,11 +65,17 @@ function App() {
     
     if(selectedCards.includes(text)){
       setSelectedCards([]);
-      setCurrScore(() => 0);
+      setCurrScore(0);
     }
     else{
       setSelectedCards([...selectedCards, text]);
-      setCurrScore(() => currScore + 1);
+
+      setCurrScore((prev) => {
+        if(prev >= bestScore){
+          setBestScore(prev + 1);
+        }
+        return currScore + 1
+      });
     }
     
     shuffleCards();
@@ -60,12 +87,15 @@ function App() {
       <header>
         <h1>Memory Card</h1>
         <div className="scoreboard-div">
-          <p>Best Score: <span className="best-score">0</span></p>
+          <p>Best Score: <span className="best-score">{bestScore}</span></p>
           <p>Current Score: <span className="curr-score"></span>{currScore}</p>
         </div>
       </header>
       <main>
-        {cards.map((cardData) => <Card data={cardData} onClick={clickCard} />)}
+        <p>Get points by clicking on an image but don't click on any more than once!</p>
+        <div>
+          {cards.map((cardData) => <Card data={cardData} onClick={clickCard} />)}
+        </div>
       </main>
     </div>
   );
